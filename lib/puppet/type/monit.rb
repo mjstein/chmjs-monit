fef33d52f05a43f7208725da6f23e9570ac453d9
mjstein <msuddd@gmail.com>
2014-07-14 14:15:48 -0700

    updating monit type with validation

--- lib/puppet/type/monit.rb
+++ lib/puppet/type/monit.rb
@@ -1,23 +1,41 @@
 Puppet::Type.newtype(:monit) do
-ensurable
-@doc = "Monitor packages through Monit"
-  newparam(:name) do
-    desc "The name of the  package monitored under monit"
-  end
-  newparam(:pidname) do
-    desc "The pid file for the process i.e. /var/run/httpd/httpd.pid"
-  end
-  newparam(:process_timeout) do
-    desc "timeout setting for a process execution"
-    defaultto 20
-    validate do |value|
-      puts value
-      if not value.is_a? Numeric 
-        raise ArgumentError,
-          "process_timeout is not a number!"
-      else
-       super
+  #ensurable
+  @doc = "Monitor packages through Monit"
+    newparam(:name) do
+      desc "The name of the  package monitored under monit"
+    end
+
+    newparam(:pid_name) do
+      desc "The pid file for the process i.e. /var/run/httpd/httpd.pid"
+      defaultto "" 
+      validate do |value|
+        if not value.is_a? String or value.empty?
+          raise ArgumentError,
+            "pid_name is not a valid string!"
+        else
+          super
+        end
+      end
+    end
+
+    newparam(:process_timeout) do
+      desc "timeout setting for a process execution"
+      defaultto 20
+      validate do |value|
+        if not value.to_i.is_a? Numeric 
+          raise ArgumentError,
+            "process_timeout is not a number!"
+        else
+         super
+      end
     end
   end
-end
+
+  autorequire(:service) do
+    'monit'
+  end
+  
+  autorequire(:package) do
+    'monit'
+  end
 end
